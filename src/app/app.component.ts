fc0ae638177aee98d29374f07f8fcc8086f2a90d
Ahmed <ghoul.ahmed5@gmail.com>
2017-11-09 22:06:31 +0100

    example with router.navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  constructor(private router: Router) {}
+  visitDefault(): void {
+    this.router.navigate([""]);
+  }
+  visitArticle(): void {
+    this.router.navigate(["article"]);
+  }
 }
